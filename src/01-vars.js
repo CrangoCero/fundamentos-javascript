ce8ea784e6d6296a00048d0582f8e7016145cdef
Crango <cristian.gomez0887@gmail.com>
2026-04-23 10:56:43 -0500

    01-Variables

---
+++ src/01-vars.js
@@ -0,0 +1,23 @@
+//variables
+//var, let, const
+
+//var es una variable global, se puede redeclarar y reasignar.
+var bloquesRojos = true;
+var bloquesRojos = 5;
+
+console.log(bloquesRojos);
+
+//let es una variable local, no se puede redeclarar.
+let contador = 0;
+let mensaje = "Hola Mundo";
+contador = 5;
+
+console.log(contador);
+console.log(mensaje);
+
+//const es una variable constante, no se puede redeclarar ni reasignar.
+const PI = 3.1416;
+
+console.log(PI);
+
+//RETOS
