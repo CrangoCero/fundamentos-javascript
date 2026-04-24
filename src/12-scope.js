81e6a05856c89ab94b1fd244a20bd3fa526f9686
Crango <cristian.gomez0887@gmail.com>
2026-04-24 17:49:00 -0500

    12-Scope

--- src/12-scope.js
+++ src/12-scope.js
@@ -1,7 +1,16 @@
 //Scope
+//El alcance de las variables en JavaScript se divide en dos tipos: global, local y de bloque. El scope determina la accesibilidad de las variables en diferentes partes del código.
 
 //Global Scope
 //Las variables declaradas fuera de cualquier bloque de código son consideradas variables globales y pueden ser accedidas desde cualquier parte de la aplicación.
+
+//Local Scope
+//Las variables declaradas dentro de un bloque de código son consideradas variables locales y solo pueden ser accedidas dentro de ese bloque.
+
+//Block Scope
+//Las variables declaradas con let o const dentro de un bloque de código (como un if, for, while, etc.) son consideradas variables de bloque y solo pueden ser accedidas dentro de ese bloque.
+
+
 
 const variableGlobal = "Soy una variable global";
 
